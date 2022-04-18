33ad7930f09539bbf3457d9b543fd66f9acbfefa
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-04-18 12:37:47 +0530

    add app bar and carousel to home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/layout/Dashboard";
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path='/me' element={<Dashboard/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Appbar from './Appbar'
+import CarouselImages from './CarouselImages'
+
+const Home = () => {
+
+
+  return (
+    <>
+      <Appbar />
+      <CarouselImages />
+    </>
+  )
+}
+
+export default Home
