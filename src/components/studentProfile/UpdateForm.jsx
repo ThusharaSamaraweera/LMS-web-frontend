1e30aa614ef7a2bfbb10379c0dd748a8a439a5bd
Radhushani <radushani111@gmail.com>
2022-05-04 20:51:05 +0530

    errors handled

--- src/components/studentProfile/UpdateForm.jsx
+++ src/components/studentProfile/UpdateForm.jsx
@@ -6,11 +6,14 @@ import validation from './validation';
 
 const UpdateForm = () => {
 
-  const initialValues = { fName: "", lName: "", email: "", studentId: "", department: "" };
+  const initialValues = { firstName: "", lastName: "", email: "radhushani@gmail.com", studentId: "", department: "" };
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
+  const [isFirstNameHasError, setIsFirstNameHasError] = useState(false);
+  const [isLastNameHasError, setIsLastNameHasError] = useState(false);
+  const [isStudentIdHasError, setIsStudentIdHasError] = useState(false);
+  const [isDepartmentHasError, setIsDepartmentHasError] = useState(false);
   const [isSubmit, setIsSubmit] = useState(false);
-
 
   const handleChange = (e) => {
 
@@ -20,13 +23,8 @@ const UpdateForm = () => {
 
   const handleReset = (e) => {
     setFormValues(initialValues);
-  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validation(formValues));
-    setIsSubmit(true);
-  };
+  }
 
   useEffect(() => {
     console.log(formErrors);
@@ -35,91 +33,146 @@ const UpdateForm = () => {
     }
   }, [formErrors]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setFormErrors(validation(formValues));
 
+    setIsFirstNameHasError(false);
+    setIsDepartmentHasError(false);
+    setIsLastNameHasError(false);
+    setIsStudentIdHasError(false);
+
+
+    if (formValues.firstName === '') {
+      setIsFirstNameHasError(true);
+    } else if (formValues.lastName === '') {
+      setIsLastNameHasError(true);
+    } else if (formValues.studentId === '') {
+      setIsStudentIdHasError(true);
+    } else if (formValues.department === '') {
+      setIsDepartmentHasError(true);
+    }
+
+    setIsSubmit(true);
+  };
+  const isDisabled = (Object.keys(formErrors).length === 0 && isSubmit);
   return (
+
     <div>
-      <Typography gutterBottom varient="h3" fontWeight="fontWeightBold" align="center">
+      <Typography
+        fontWeight="fontWeightBold"
+        align="center"
+        sx={{
+          fontSize: {
+            xs: "1rem",
+            sm: "2rem",
+          }
+        }}>
         Student - Profile
       </Typography>
-
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div>Profile Updated successfully</div>
-      ) : (<div>Update your profile</div>)}
+      
 
       <Card>
         <CardContent>
 
           <form onSubmit={handleSubmit}>
-            <Grid container spacing={1}>
+            <Grid container sx={{width:"auto"}}spacing={3} >
+            
               <Grid xs={12} sm={6} item>
                 <TextField
+                  error={isFirstNameHasError}
+                  id="standard-error-helper-text"
+                  helperText={formErrors.firstName}
                   label="First Name"
-                  name="fName"
-                  placeholder="Enter first name"
-                  value={formValues.fName}
+                  name="firstName"
+                  placeholder="Enter First Name"
+                  value={formValues.firstName}
                   onChange={handleChange}
-                  varient="outlined"
+                  InputProps={{
+                    readOnly: isDisabled,
+                  }}
+                  variant="standard"
                   fullWidth
-                  required />
+                />
               </Grid>
 
               <Grid xs={12} sm={6} item>
                 <TextField
+                  error={isLastNameHasError}
+                  id="standard-error-helper-text"
+                  helperText={formErrors.lastName}
                   label="Last Name"
-                  name="lName"
-                  placeholder="Enter last name"
-                  value={formValues.lName}
+                  name="lastName"
+                  placeholder="Enter Last Name"
+                  value={formValues.lastName}
                   onChange={handleChange}
-                  varient="outlined"
-                  fullWidth
-                  required />
+                  InputProps={{
+                    readOnly: isDisabled,
+                  }}
+                  variant="standard"
+                  fullWidth />
               </Grid>
-
 
               <Grid xs={12} item>
                 <TextField
                   type="email"
+                  id="standard-read-only-input"
                   label="Email"
                   name="email"
                   placeholder="Enter email"
                   value={formValues.email}
-                  onChange={handleChange}
-                  variant="outlined"
-                  fullWidth required />
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  variant="standard"
+                  fullWidth
+                />
               </Grid>
 
 
               <Grid xs={12} item>
                 <TextField
+                  error={isStudentIdHasError}
+                  id="standard-error-helper-text"
+                  helperText={formErrors.studentId}
                   label="Student ID"
                   name="studentId"
                   placeholder="Enter Student ID"
                   value={formValues.studentId}
                   onChange={handleChange}
-                  variant="outlined"
-                  fullWidth required />
+                  InputProps={{
+                    readOnly: isDisabled,
+                  }}
+                  variant="standard"
+                  fullWidth />
               </Grid>
 
 
               <Grid xs={12} item>
                 <TextField
+                  error={isDepartmentHasError}
+                  id="standard-error-helper-text"
+                  helperText={formErrors.department}
                   label="Department"
                   name="department"
                   placeholder="Enter department"
                   value={formValues.department}
                   onChange={handleChange}
-                  variant="outlined"
-                  fullWidth
-                  required />
-              </Grid>
+                  variant="standard"
+                  InputProps={{
+                    readOnly: isDisabled,
+                  }}
 
+                  fullWidth
+                />
+              </Grid>
 
               <Grid xs={12} sm={6} item>
                 <Button
                   type="submit"
                   variant="contained"
                   color="primary"
-                  disabled={Object.keys(formErrors).length === 0 && isSubmit}
+                  disabled={isDisabled}
                 >
                   Submit
                 </Button>
@@ -130,9 +183,9 @@ const UpdateForm = () => {
                   type="reset"
                   variant="contained"
                   color="primary"
-                  disabled={Object.keys(formErrors).length === 0 && isSubmit}
+                  disabled={isDisabled}
                   onClick={handleReset}
-                  >
+                >
                   Reset the form
                 </Button>
               </Grid>
--- src/components/studentProfile/validation.jsx
+++ src/components/studentProfile/validation.jsx
@@ -1,11 +1,12 @@
 const validation = (values) => {
     const errors = {};
+
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.fName) {
-      errors.fName = "Firstname is required!";
+    if (!values.firstName) {
+      errors.firstName = "Firstname is required!";
     }
-    if (!values.lName) {
-        errors.fName = "Lastname is required!";
+    if (!values.lastName) {
+        errors.lastName = "Lastname is required!";
     }
     if (!values.department) {
         errors.department = "Department is required!";
@@ -13,11 +14,7 @@ const validation = (values) => {
     if (!values.studentId) {
         errors.studentId = "StudentId is required!";
     }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
+    
     return errors;
 };
 
