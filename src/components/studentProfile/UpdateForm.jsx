21c442d4628fd26f2f354aabd1b610ba0ca16be3
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-05-05 22:00:05 +0530

    change error handling login

--- src/components/studentProfile/UpdateForm.jsx
+++ src/components/studentProfile/UpdateForm.jsx
@@ -1,66 +1,96 @@
-import { CardContent, Typography, Card, TextField, Grid, Button, FormControl, InputLabel, Select, MenuItem, Box } from '@mui/material'
-import React from 'react'
-import { useState, useEffect } from 'react';
-import validation from './validation';
+import {
+  CardContent,
+  Typography,
+  Card,
+  TextField,
+  Grid,
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  FormHelperText,
+} from "@mui/material";
+import React from "react";
+import { useState, useEffect } from "react";
 
-const department = [
-  "SE", "PS", "PE"
-]
+const department = ["SE", "PS", "PE"];
 
 const UpdateForm = () => {
-
-  const initialValues = { firstName: "", lastName: "", email: "radhushani@gmail.com", studentId: "", department: "" };
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "radhushani@gmail.com",
+    studentId: "",
+    department: "",
+  };
   const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isFirstNameHasError, setIsFirstNameHasError] = useState(false);
-  const [isLastNameHasError, setIsLastNameHasError] = useState(false);
-  const [isStudentIdHasError, setIsStudentIdHasError] = useState(false);
-  const [isDepartmentHasError, setIsDepartmentHasError] = useState(false);
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [studentIdError, setStudentIdError] = useState("");
+  const [isDepartmentError, setDepartmentError] = useState("");
   const [isSubmit, setIsSubmit] = useState(false);
   const [isDisabled, setDisabled] = useState(false);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
   const handleReset = (e) => {
     setFormValues(initialValues);
-  }
-
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors]);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setFormErrors(validation(formValues));
 
-    setIsFirstNameHasError(false);
-    setIsDepartmentHasError(false);
-    setIsLastNameHasError(false);
-    setIsStudentIdHasError(false);
-
-
-    if (formValues.firstName === '') {
-      setIsFirstNameHasError(true);
-    } else if (formValues.lastName === '') {
-      setIsLastNameHasError(true);
-    } else if (formValues.studentId === '') {
-      setIsStudentIdHasError(true);
-    } else if (formValues.department === '') {
-      setIsDepartmentHasError(true);
+    if (formValues.firstName === "") {
+      setFirstNameError("First name is required");
+    }
+    if (formValues.lastName === "") {
+      setLastNameError("Last name is required");
+    }
+    if (formValues.studentId === "") {
+      setStudentIdError("Student id is required");
+    }
+    if (formValues.department === "") {
+      setDepartmentError("Department is required");
     }
 
     setIsSubmit(true);
   };
 
-  const renderDepartment = department.map( (department) => {
-    return <MenuItem value={department}>{department}</MenuItem>
-  })
+  const handleOnFirstNameChange = (e) => {
+    const { value } = e.target;
+    if (value) {
+      setFirstNameError("");
+    }
+    setFormValues({ ...formValues, firstName: value });
+  };
+
+  const handleOnLastNameChange = (e) => {
+    const { value } = e.target;
+    if (value) {
+      setLastNameError("");
+    }
+    setFormValues({ ...formValues, lastName: value });
+  };
+
+  const handleOnStudentIDChange = (e) => {
+    const { value } = e.target;
+    if (value) {
+      setStudentIdError("");
+    }
+    setFormValues({ ...formValues, studentId: value });
+  };
+
+  const handleOnDepartmentChange = (e) => {
+    const { value } = e.target;
+    if (value) {
+      setDepartmentError("");
+    }
+    setFormValues({ ...formValues, department: value });
+  };
+
+  const renderDepartment = department.map((department) => {
+    return <MenuItem key={department} value={department}>{department}</MenuItem>;
+  });
 
   return (
     <Box>
@@ -71,26 +101,26 @@ const UpdateForm = () => {
           fontSize: {
             xs: "1rem",
             sm: "2rem",
-          }
-        }}>
+          },
+        }}
+      >
         Student - Profile
       </Typography>
-      
+
       <Card>
         <CardContent>
           <form onSubmit={handleSubmit}>
-            <Grid container sx={{width:"auto"}}spacing={3} >
-            
+            <Grid container sx={{ width: "auto" }} spacing={3}>
               <Grid xs={12} sm={6} item>
                 <TextField
-                  error={isFirstNameHasError}
+                  error={Boolean(firstNameError)}
                   id="standard-error-helper-text"
-                  helperText={formErrors.firstName}
+                  helperText={firstNameError}
                   label="First Name"
                   name="firstName"
                   placeholder="Enter First Name"
                   value={formValues.firstName}
-                  onChange={handleChange}
+                  onChange={handleOnFirstNameChange}
                   variant="standard"
                   fullWidth
                   disabled={isDisabled}
@@ -99,17 +129,18 @@ const UpdateForm = () => {
 
               <Grid xs={12} sm={6} item>
                 <TextField
-                  error={isLastNameHasError}
+                  error={Boolean(lastNameError)}
                   id="standard-error-helper-text"
-                  helperText={formErrors.lastName}
+                  helperText={lastNameError}
                   label="Last Name"
                   name="lastName"
                   placeholder="Enter Last Name"
                   value={formValues.lastName}
-                  onChange={handleChange}
+                  onChange={handleOnLastNameChange}
                   disabled={isDisabled}
                   variant="standard"
-                  fullWidth />
+                  fullWidth
+                />
               </Grid>
 
               <Grid xs={12} item>
@@ -126,69 +157,83 @@ const UpdateForm = () => {
                 />
               </Grid>
 
-
               <Grid xs={12} item>
                 <TextField
-                  error={isStudentIdHasError}
+                  error={Boolean(studentIdError)}
                   id="standard-error-helper-text"
-                  helperText={formErrors.studentId}
+                  helperText={studentIdError}
                   label="Student ID"
                   name="studentId"
                   placeholder="Enter Student ID"
                   value={formValues.studentId}
-                  onChange={handleChange}
+                  onChange={handleOnStudentIDChange}
                   disabled={isDisabled}
                   variant="standard"
-                  fullWidth />
+                  fullWidth
+                />
               </Grid>
 
               <Grid xs={12} item>
                 <FormControl fullWidth>
-                  <InputLabel id="department-select-label">Department</InputLabel>
+                  <InputLabel id="department-select-label">
+                    Department
+                  </InputLabel>
                   <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
+                    labelId="department-select-label"
+                    id="department-select"
                     value={formValues.department}
                     label="department"
-                    name='department'
-                    onChange={handleChange}
-                    variant='standard'
+                    name="department"
+                    onChange={handleOnDepartmentChange}
+                    variant="standard"
                     placeholder="Enter department"
+                    error={Boolean(isDepartmentError)}
                   >
-                  {renderDepartment}
+                    {renderDepartment}
                   </Select>
+                  <FormHelperText id="department-error">
+                    {isDepartmentError && "Department is required"}
+                  </FormHelperText>
                 </FormControl>
               </Grid>
 
-              <Grid xs={12} sm={6} item>
+              <Grid xs={12} sm={12} item
+                sx={{
+                  display: "flex",
+                  justifyContent: "center"
+                }}
+              >
                 <Button
                   type="submit"
                   variant="contained"
                   color="primary"
                   disabled={isDisabled}
+                  sx={{
+                    marginX: 2
+                  }}
                 >
                   Submit
                 </Button>
-              </Grid>
-
-              <Grid xs={12} sm={6} item>
                 <Button
                   type="reset"
                   variant="contained"
                   color="primary"
                   disabled={isDisabled}
                   onClick={handleReset}
+                  sx={{
+                    marginX: 2
+                  }}
                 >
-                  Reset the form
+                  Reset
                 </Button>
               </Grid>
-            </Grid>
 
+            </Grid>
           </form>
         </CardContent>
       </Card>
     </Box>
-  )
-}
+  );
+};
 
 export default UpdateForm;
