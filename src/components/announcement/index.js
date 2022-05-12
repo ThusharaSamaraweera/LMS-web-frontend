b6ec4709e1a20b8dacdfd0154932b6e3fc462836
Radhushani <radushani111@gmail.com>
2022-05-12 22:28:13 +0530

    Add announcement button created

--- src/components/announcement/index.js
+++ src/components/announcement/index.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Box, Container, Stack, Typography, Button } from "@mui/material";
+import { useState } from "react";
 import AnnouncementForm from './AnnouncementForm'
 
 const AnnouncementManagement = () => {
+  const [isAnnouncementFormVisible, setAnnouncemntFormVisible] = useState(false);
+
+  const handleOnAddAnnouncementBtnClick = () => {
+    
+  }
   return (
-    <div>
-     <AnnouncementForm/>
-    </div>
+    <Container>
+      <Typography variant="h5" >Make Announcement</Typography>
+      <Stack>      
+        <Stack
+          spacing={2}
+          sx={{
+            direction: "row",
+            marginY: 2,
+          }}
+        >
+          <Button variant="outlined" size="large" onClick={handleOnAddAnnouncementBtnClick}>
+            {isAnnouncementFormVisible ? "Collapse form" : "Add Announcement"}
+          </Button>
+        </Stack>
+
+        <Box 
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginY: 2,
+          }}
+        >
+          { isAnnouncementFormVisible && <AnnouncementForm />}
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
