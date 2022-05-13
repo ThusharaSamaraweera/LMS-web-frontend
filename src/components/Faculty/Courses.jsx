89387b0a5d266f8d93d5afd2d7ad04df29a1e57b
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-05-14 04:35:59 +0530

    add confirmation dialog box to course enrollment

--- src/components/Faculty/Courses.jsx
+++ src/components/Faculty/Courses.jsx
@@ -2,12 +2,16 @@ import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import { Collapse } from "antd";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Alert from "../utilsComponents/Alert";
+import ConfirmationDialog from "../utilsComponents/ConfirmationDialog";
+import { useState } from "react";
 
 const { Panel } = Collapse;
 
 const Courses = () => {
   const { department } = useParams();
   const allCourses = useSelector((state) => state.courseReducer.courses);
+  const [isConfirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
 
   const categorizeCourses = (level, semester) => {
     return allCourses.filter(
@@ -16,6 +20,23 @@ const Courses = () => {
         parseInt(course.course_id[5]) === semester &&
         course.department_name.toLowerCase() === department.toLowerCase()
     );
+  };
+
+  const handleOnAccept = () => {
+    console.log("accept");
+    setConfirmationDialogOpen(false);
+    setTimeout(() => {
+      Alert({ message: "Enrolled", type: "success" });
+    }, 800);
+  };
+
+  const handleOnCancel = () => {
+    console.log("cancel");
+    setConfirmationDialogOpen(false);
+  };
+
+  const handleOnClickEnroll = () => {
+    setConfirmationDialogOpen(true);
   };
 
   const renderCourses = (level, semester) => {
@@ -33,78 +54,91 @@ const Courses = () => {
             <Typography>{course.course_name}</Typography>
           </Grid>
           <Grid item xs={12} sm={3}>
-            <Button variant="outlined" size="small">
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={handleOnClickEnroll}
+            >
               Enroll me
             </Button>
           </Grid>
         </Grid>
       );
     });
-  } 
+  };
 
   return (
-    <Collapse defaultActiveKey={[1, 2, 3, 4]}>
-      <Panel header="Level 1" key="1">
-        <Collapse>
-          <Panel header="Semester 1" key="1">
-            <Box>
-              <Stack>{renderCourses(1, 1)}</Stack>
-            </Box>
-          </Panel>
-          <Panel header="Semester 1" key="2">
-            <Box>
-              <Stack>{renderCourses(1, 2)}</Stack>
-            </Box>          
-          </Panel>
-        </Collapse>
-      </Panel>
+    <>
+      {isConfirmationDialogOpen && (
+        <ConfirmationDialog
+          title={"Do you want to enroll?"}
+          handleOnAccept={handleOnAccept}
+          handleOnCancel={handleOnCancel}
+        />
+      )}
+      <Collapse defaultActiveKey={[1, 2, 3, 4]}>
+        <Panel header="Level 1" key="1">
+          <Collapse>
+            <Panel header="Semester 1" key="1">
+              <Box>
+                <Stack>{renderCourses(1, 1)}</Stack>
+              </Box>
+            </Panel>
+            <Panel header="Semester 1" key="2">
+              <Box>
+                <Stack>{renderCourses(1, 2)}</Stack>
+              </Box>
+            </Panel>
+          </Collapse>
+        </Panel>
 
-      <Panel header="Level 2" key="2">
-        <Collapse>
-          <Panel header="Semester 1" key="1">
-            <Box>
-              <Stack>{renderCourses(2, 1)}</Stack>
-            </Box>
-          </Panel>
-          <Panel header="Semester 1" key="2">
-            <Box>
-              <Stack>{renderCourses(2, 2)}</Stack>
-            </Box> 
-          </Panel>
-        </Collapse>
-      </Panel>
+        <Panel header="Level 2" key="2">
+          <Collapse>
+            <Panel header="Semester 1" key="1">
+              <Box>
+                <Stack>{renderCourses(2, 1)}</Stack>
+              </Box>
+            </Panel>
+            <Panel header="Semester 1" key="2">
+              <Box>
+                <Stack>{renderCourses(2, 2)}</Stack>
+              </Box>
+            </Panel>
+          </Collapse>
+        </Panel>
 
-      <Panel header="Level 3" key="3">
-        <Collapse>
-          <Panel header="Semester 1" key="1">
-            <Box>
-              <Stack>{renderCourses(3, 1)}</Stack>
-            </Box> 
-          </Panel>
-          <Panel header="Semester 1" key="2">
-            <Box>
-              <Stack>{renderCourses(3, 2)}</Stack>
-            </Box> 
-          </Panel>
-        </Collapse>
-      </Panel>
+        <Panel header="Level 3" key="3">
+          <Collapse>
+            <Panel header="Semester 1" key="1">
+              <Box>
+                <Stack>{renderCourses(3, 1)}</Stack>
+              </Box>
+            </Panel>
+            <Panel header="Semester 1" key="2">
+              <Box>
+                <Stack>{renderCourses(3, 2)}</Stack>
+              </Box>
+            </Panel>
+          </Collapse>
+        </Panel>
 
-      <Panel header="Level 4" key="4">
-        <Collapse>
-          <Panel header="Semester 1" key="1">
-            <Box>
-              <Stack>{renderCourses(4, 1)}</Stack>
-            </Box>
-          </Panel>
-          <Panel header="Semester 1" key="2">
-            <Box>
-              <Stack>{renderCourses(4, 2)}</Stack>
-            </Box> 
-          </Panel>
-        </Collapse>
-      </Panel>
-    </Collapse>
-  )
+        <Panel header="Level 4" key="4">
+          <Collapse>
+            <Panel header="Semester 1" key="1">
+              <Box>
+                <Stack>{renderCourses(4, 1)}</Stack>
+              </Box>
+            </Panel>
+            <Panel header="Semester 1" key="2">
+              <Box>
+                <Stack>{renderCourses(4, 2)}</Stack>
+              </Box>
+            </Panel>
+          </Collapse>
+        </Panel>
+      </Collapse>
+    </>
+  );
 };
 
 export default Courses;
