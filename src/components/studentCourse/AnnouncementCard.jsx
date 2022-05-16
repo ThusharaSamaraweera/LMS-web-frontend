19f07cec75a19e74c4a1a39a2999ea6ea7e2b8fc
Radhushani <radushani111@gmail.com>
2022-05-16 21:39:56 +0530

    academic year got from course reducer

--- src/components/studentCourse/AnnouncementCard.jsx
+++ src/components/studentCourse/AnnouncementCard.jsx
@@ -8,11 +8,19 @@ const AnnouncementCard = () => {
   const { Panel } = Collapse;
   const { courseId } = useParams();
 
-  
+  const courses = useSelector((state) => state.courseReducer.courses);
+
+  var academicYear = "";
+  courses.map(course=>{
+      if(course.course_id=== courseId){
+        academicYear = course.academic_year
+      }
+    }
+  );
 
   const course = {
     category: courseId,
-    academicYear: "2018/2019"
+    academicYear: academicYear
   }
 
   const [notificationValues, setNotificationValues] = useState([]);
