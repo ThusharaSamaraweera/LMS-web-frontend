b0ead9a9498604538b18bc6f792eb4821c664eef
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-05-17 13:43:52 +0530

    add announcement section in lecturer course

---
+++ src/components/lecturerCourse/AnnouncementSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnnouncementSection = () => {
+  return (
+    <div>AnnouncementSection</div>
+  )
+}
+
+export default AnnouncementSection
--- src/components/lecturerCourse/index.jsx
+++ src/components/lecturerCourse/index.jsx
@@ -1,13 +1,17 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AddGradeTable from "./AddGradeTable";
 import StudentService from '../../services/student.service'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+import AnnouncementSection from "./AnnouncementSection";
 
 const LecturerCourse = () => {
   const { courseId } = useParams();
   const [isGradeTableVisible, setGradeTableVisible] = useState(false);
   const [courseDetails, setCourseDetails] = useState({})
+  const [isAnnoucementSectionOpen, setAnnouncementOpen] = useState(false);
 
   const handleOnAddGradeBtnClick = () => {
     setGradeTableVisible(!isGradeTableVisible);
@@ -19,18 +23,22 @@ const LecturerCourse = () => {
     });
   };
 
+  const handleOnToggleAnnoucementBtn = () => {
+    setAnnouncementOpen(!isAnnoucementSectionOpen);
+  };
+
+
   useEffect(() => {
     fetchCourseDetail();
   }, [courseId]);
 
   return (
-    <div>
+    <Container>
       <Box sx={{ display: "flex", justifyContent: "end", marginY: 2 }}>
         <Button variant="outlined" onClick={handleOnAddGradeBtnClick}>
           { isGradeTableVisible ?  "Collapse table" : "Add grades"}
         </Button>
       </Box>
-      <Box>{isGradeTableVisible && <AddGradeTable courseId={courseId} />}</Box>
       <Grid
         container
         sx={{
@@ -44,8 +52,48 @@ const LecturerCourse = () => {
             <Typography>Lecturer : {courseDetails.lecturer}</Typography>
           </Box>
         </Grid>
+
+        <Grid
+          item
+          xs={12}
+          sm={4}
+          sx={{
+            textAlign: "end",
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          <Button
+            variant="outlined"
+            // onClick={handleOnClickUnenroll}
+            sx={{
+              margin: 1,
+            }}
+            size="small"
+            startIcon={<ExitToAppIcon/>}
+          >
+            Delete
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={handleOnToggleAnnoucementBtn}
+            sx={{
+              margin: 1,
+            }}
+            size="small"
+            startIcon={<AnnouncementIcon/>}
+          >
+            {isAnnoucementSectionOpen ? "Close Announcement" : "Announcement"}
+          </Button>
+        </Grid>
       </Grid>
-    </div>
+
+      {isAnnoucementSectionOpen && (
+        <AnnouncementSection/>
+      )}
+      <Box>{isGradeTableVisible && <AddGradeTable courseId={courseId} />}</Box>
+
+    </Container>
   );
 };
 
